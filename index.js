5324de63d5ff798efe940e457636722a70ed0916
Alex Whitman <alex@alexwhitman.com>
2013-04-20 17:41:28 +0100

    Convert to jarvis api

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+/*global module,require*/
+
 var sandbox = require('sandbox');
 
-module.exports = function(bot, module) {
+module.exports = function(jarvis, module) {
 
 	var s = new sandbox();
 
-	module.addCommand({
+	module.addAction(module.createCommand({
 		match: 'eval :code',
-		func: function(request, code) {
+		func: function(message, code) {
 			s.run(code, function(output) {
-				request.reply = output.result;
-				bot.reply(request);
+				jarvis.reply(message, output.result);
 			});
 		}
-	})
+	}));
 
 };
